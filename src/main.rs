17dd0af142236ddc9f72ff3ea3da18c5a9700865
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-05 13:38:24 +1000

    start day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod util;
 fn main() {
     day1::part1(util::get_from_file(1));
@@ -14,4 +15,9 @@ fn main() {
     day3::part1(util::get_from_file(3));
     day3::part2(util::get_test_from_file(3));
     day3::part2(util::get_from_file(3));
+
+    day4::part1(util::get_test_from_file(4));
+    // day4::part1(util::get_from_file(4));
+    day4::part2(util::get_test_from_file(4));
+    // day4::part2(util::get_from_file(4));
 }
