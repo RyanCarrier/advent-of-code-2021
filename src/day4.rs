499182a197b113721525c294a249c4750e4b00c0
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-06 02:45:46 +1000

    done start of day4

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,70 @@
+use core::panic;
+
+#[derive(Debug, Clone)]
+struct State {
+    draws: Vec<u64>,
+    n: usize,
+    boards: Vec<Vec<Vec<u64>>>,
+}
+
+impl State {
+    fn check_board(&self, draw_depth: usize, board: usize) -> bool {
+        let draws = self.draws.get(0..draw_depth).unwrap();
+        'x: for x in 0..self.n {
+            for y in 0..self.n {
+                let board_item = self
+                    .boards
+                    .get(board)
+                    .unwrap()
+                    .get(x)
+                    .unwrap()
+                    .get(y)
+                    .unwrap();
+                if !draws.contains(board_item) {
+                    continue 'x;
+                }
+            }
+            println!("xwin");
+            return true;
+        }
+        'y: for y in 0..self.n {
+            for x in 0..self.n {
+                let board_item = self
+                    .boards
+                    .get(board)
+                    .unwrap()
+                    .get(x)
+                    .unwrap()
+                    .get(y)
+                    .unwrap();
+                if !draws.contains(board_item) {
+                    continue 'y;
+                }
+            }
+            println!("ywin");
+            return true;
+        }
+        return false;
+    }
+}
+
 pub fn part1(lines: Vec<String>) {
+    let state = import(lines);
+    println!("\tdraws:{:?}", state.draws);
+    println!("\tn:{:?}", state.n);
+    println!("\tboard:{:?}", state.boards);
+    'drawn: for drawn in 0..state.draws.len() {
+        for board in 0..state.boards.len() - 1 {
+            if state.check_board(drawn, board) {
+                println!(
+                    "board #{:?} won, draws:{:?}",
+                    board,
+                    state.clone().draws.get(0..drawn).unwrap(),
+                );
+                break 'drawn;
+            }
+        }
+    }
     println!("day4part1: ")
 }
 
@@ -8,10 +74,38 @@ pub fn part2(lines: Vec<String>) {
     }
 }
 
-fn import(lines: Vec<String>) {
+fn import<'a>(lines: Vec<String>) -> State {
     let draws = import_draws(lines.get(0).unwrap());
-    let temp: Vec<_> = lines.get(2).unwrap().trim().split(" ").collect();
+    let temp: Vec<_> = lines
+        .get(2)
+        .unwrap()
+        .trim()
+        .split(" ")
+        .filter(|x| x.len() > 0)
+        .collect();
     let n: usize = temp.len();
+    let mut i = 1;
+    let mut boards: Vec<Vec<Vec<u64>>> = vec![];
+    loop {
+        match lines.get(i) {
+            None => {
+                return State {
+                    draws,
+                    n,
+                    boards: boards,
+                }
+            }
+            Some(x) => {
+                if x.len() == 0 {
+                    panic!("err len should not be 0");
+                }
+                let got_lines = lines.get(i..i + n).unwrap().to_vec();
+                let temp = import_board(got_lines);
+                boards.push(temp);
+            }
+        }
+        i += n;
+    }
 }
 
 fn import_draws(line: &String) -> Vec<u64> {
@@ -19,5 +113,14 @@ fn import_draws(line: &String) -> Vec<u64> {
 }
 
 fn import_board(lines: Vec<String>) -> Vec<Vec<u64>> {
-    return vec![vec![123]];
+    lines
+        .into_iter()
+        .map(|x| {
+            x.trim()
+                .split(" ")
+                .filter(|y| y.len() > 0)
+                .map(|y| y.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
