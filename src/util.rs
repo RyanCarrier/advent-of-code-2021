f9f9c1de502f7c0adf4247fbc5f296ea50e3f5c6
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-08 22:00:00 +1000

    update to work with windows line endings

--- src/util.rs
+++ src/util.rs
@@ -1,25 +1,19 @@
 use std::fs;
 
 pub fn get_from_file(day: i32) -> Vec<String> {
-    let filename = format!("./inputs/day{}.txt", day);
-
-    let lines: Vec<String> = fs::read_to_string(filename)
-        .unwrap()
-        .split('\n')
-        .filter(|x: &&str| x.trim() != "")
-        .map(|x| String::from(x))
-        .collect();
-    lines
+    get_from_filename(format!("./inputs/day{}.txt", day))
 }
 
 pub fn get_test_from_file(day: i32) -> Vec<String> {
-    let filename = format!("./inputs/day{}test.txt", day);
+    get_from_filename(format!("./inputs/day{}test.txt", day))
+}
 
+fn get_from_filename(filename: String) -> Vec<String> {
     let lines: Vec<String> = fs::read_to_string(filename)
         .unwrap()
         .split('\n')
         .filter(|x: &&str| x.trim() != "")
-        .map(|x| String::from(x))
+        .map(|x| String::from(x.trim()))
         .collect();
     lines
 }
