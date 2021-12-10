1f9aad6b89571767bba1259fee2eb06aab9a9d26
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-10 17:26:34 +1000

    complete day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,14 +1,72 @@
+use std::collections::HashMap;
 
+struct Navline {
+    line: String,
+}
+
+impl Navline {
+    fn corrupt(&self) -> usize {
+        let scores: HashMap<char, usize> =
+            HashMap::from([('}', 1197), (']', 57), (')', 3), ('>', 25137)]);
+        let convert: HashMap<char, char> =
+            HashMap::from([('{', '}'), ('[', ']'), ('(', ')'), ('<', '>')]);
+        let mut stack: Vec<char> = vec![];
+        for c in self.line.chars() {
+            if convert.contains_key(&c) {
+                stack.push(*convert.get(&c).unwrap())
+            } else if *stack.last().unwrap() == c {
+                stack.pop();
+                continue;
+            } else {
+                return *scores.get(&c).unwrap();
+            }
+        }
+        return 0;
+    }
+    fn autocomplete(&self) -> usize {
+        let scores: HashMap<char, usize> = HashMap::from([('}', 3), (']', 2), (')', 1), ('>', 4)]);
+        let convert: HashMap<char, char> =
+            HashMap::from([('{', '}'), ('[', ']'), ('(', ')'), ('<', '>')]);
+        let mut stack: Vec<char> = vec![];
+        for c in self.line.chars() {
+            if convert.contains_key(&c) {
+                stack.push(*convert.get(&c).unwrap())
+            } else if *stack.last().unwrap() == c {
+                stack.pop();
+                continue;
+            }
+        }
+        let mut total = 0;
+        while let Some(next) = stack.pop() {
+            total *= 5;
+            total += scores.get(&next).unwrap()
+        }
+        total
+    }
+}
 pub fn part1(lines: Vec<String>) {
     let data = import(lines);
-    println!("day9part1: {}", data.len())
+
+    println!(
+        "day9part1: {}",
+        data.iter().fold(0, |total, nl| total + nl.corrupt())
+    )
 }
 
 pub fn part2(lines: Vec<String>) {
     let data = import(lines);
-    println!("day9part2: {}", data.len());
+    let clean_data: Vec<Navline> = data.into_iter().filter(|x| x.corrupt() == 0).collect();
+    let mut scores: Vec<usize> = clean_data.iter().map(|x| x.autocomplete()).collect();
+    scores.sort();
+
+    println!("day9part2: {}", scores[scores.len() / 2]);
 }
 
-fn import(lines: Vec<String>) -> Vec<Vec<usize>> {
-    lines.iter().map(|x| x.split(" ").map(|y| y.parse::<usize>().unwrap()).collect()).collect()
+fn import(lines: Vec<String>) -> Vec<Navline> {
+    lines
+        .iter()
+        .map(|x| Navline {
+            line: String::from(x),
+        })
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -9,41 +10,48 @@ mod day8;
 mod day9;
 mod util;
 fn main() {
-    day1::part1(util::get_from_file(1));
-    day1::part2(util::get_from_file(1));
+    if false {
+        day1::part1(util::get_from_file(1));
+        day1::part2(util::get_from_file(1));
 
-    day2::part1(util::get_from_file(2));
-    day2::part2(util::get_from_file(2));
-    day2::part2(util::get_test_from_file(2));
+        day2::part1(util::get_from_file(2));
+        day2::part2(util::get_from_file(2));
+        day2::part2(util::get_test_from_file(2));
 
-    day3::part1(util::get_test_from_file(3));
-    day3::part1(util::get_from_file(3));
-    day3::part2(util::get_test_from_file(3));
-    day3::part2(util::get_from_file(3));
+        day3::part1(util::get_test_from_file(3));
+        day3::part1(util::get_from_file(3));
+        day3::part2(util::get_test_from_file(3));
+        day3::part2(util::get_from_file(3));
 
-    day4::part1(util::get_test_from_file(4));
-    day4::part1(util::get_from_file(4));
-    day4::part2(util::get_from_file(4));
+        day4::part1(util::get_test_from_file(4));
+        day4::part1(util::get_from_file(4));
+        day4::part2(util::get_from_file(4));
 
-    day5::part1(util::get_test_from_file(5));
-    day5::part1(util::get_from_file(5));
-    day5::part2(util::get_from_file(5));
+        day5::part1(util::get_test_from_file(5));
+        day5::part1(util::get_from_file(5));
+        day5::part2(util::get_from_file(5));
 
-    day6::part1_test(util::get_test_from_file(6));
-    day6::part1(util::get_from_file(6));
-    day6::part2(util::get_from_file(6));
+        day6::part1_test(util::get_test_from_file(6));
+        day6::part1(util::get_from_file(6));
+        day6::part2(util::get_from_file(6));
 
-    day7::part1(util::get_test_from_file(7));
-    day7::part1(util::get_from_file(7));
-    day7::part2(util::get_from_file(7));
+        day7::part1(util::get_test_from_file(7));
+        day7::part1(util::get_from_file(7));
+        day7::part2(util::get_from_file(7));
 
-    day8::part1(util::get_test_from_file(8));
-    day8::part1(util::get_from_file(8));
-    day8::part2(util::get_test_from_file(88));
-    day8::part2(util::get_from_file(8));
+        day8::part1(util::get_test_from_file(8));
+        day8::part1(util::get_from_file(8));
+        day8::part2(util::get_test_from_file(88));
+        day8::part2(util::get_from_file(8));
 
-    day9::part1(util::get_test_from_file(9));
-    day9::part1(util::get_from_file(9));
-    day9::part2(util::get_test_from_file(9));
-    day9::part2(util::get_from_file(9));
+        day9::part1(util::get_test_from_file(9));
+        day9::part1(util::get_from_file(9));
+        day9::part2(util::get_test_from_file(9));
+        day9::part2(util::get_from_file(9));
+    }
+
+    day10::part1(util::get_test_from_file(10));
+    day10::part1(util::get_from_file(10));
+    day10::part2(util::get_test_from_file(10));
+    day10::part2(util::get_from_file(10));
 }
