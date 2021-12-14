5a050cc789e8be1188cb561dcf8fae075cc8c7a3
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-14 19:20:46 +1000

    start freq analysis

--- src/day14.rs
+++ src/day14.rs
@@ -1,34 +1,64 @@
+use std::io::{Read, Write};
+
 // #[derive(Clone, Copy)]
 struct Day14 {
     elements: Vec<u8>,
     pairs: Vec<Pair>,
+    n: usize,
 }
 
 struct Pair {
     at: [u8; 2],
     insert: u8,
 }
+struct PairFreq {
+    pairs: Vec<u8>,
+    inserts: Vec<u8>,
+}
+impl Pair {
+    fn to_string(&self) -> String {
+        format!("{}{} -> {}", self.at[0], self.at[1], self.insert)
+    }
+}
 
 impl Day14 {
+    fn freq_anal(&self) -> PairFreq {
+        let mut pf = PairFreq {
+            pairs: vec![0; self.n],
+            inserts: vec![0; self.n],
+        };
+        for p in &self.pairs {
+            pf.pairs[p.at[0] as usize] += 1;
+            pf.pairs[p.at[1] as usize] += 1;
+            pf.inserts[p.insert as usize] += 1;
+        }
+        pf
+    }
     fn to_string(&self) -> String {
-        let freq = self
-            .elements
-            .iter()
-            .fold([0; 28], |mut freq: [usize; 28], e| {
-                freq[*e as usize] += 1;
-                freq
-            });
+        let freq: Vec<usize> =
+            self.elements
+                .iter()
+                .fold(vec![0; self.n], |mut freq: Vec<usize>, e| {
+                    freq[*e as usize] += 1;
+                    freq
+                });
         format!("{:?}", freq)
     }
     fn steps(&mut self, n: usize) {
         (0..n).for_each(|_| self.step());
     }
-    fn steps_new(&mut self, n: usize) {
+
+    fn steps_new_broken(&mut self, n: usize) {
         let mut new: Vec<u8> = vec![];
         let mut i = 0;
-        new.push(self.elements[0]);
-        while i < self.elements.len() - 1 {
-            new.push(self.elements[i + 1]);
+        let mut ie = 0;
+        new.push(self.elements[ie]);
+        ie += 1;
+        while i < new.len() {
+            if i == new.len() - 1 && ie < self.elements.len() {
+                new.push(self.elements[ie]);
+                ie += 1;
+            }
             'n: for _ in 0..n {
                 for p in &self.pairs {
                     if p.at[0] == new[i] && p.at[1] == new[i + 1] {
@@ -44,7 +74,9 @@ impl Day14 {
         self.elements = new;
     }
     #[allow(dead_code)]
-    fn step(&mut self) {
+    fn step2(&mut self) {
+        //12s @ 17steps
+        //3s @ 16steps
         let mut i = 0;
         'e: while i < self.elements.len() - 1 {
             for p in &self.pairs {
@@ -59,18 +91,31 @@ impl Day14 {
     }
 
     #[allow(dead_code)]
-    fn step_older(&mut self) {
-        let mut new: Vec<u8> = vec![];
+    fn step(&mut self) {
+        //88ms @ 16step
+        //43s @ 25step
+        //new 25step:33s
+        //new 25step:30s
+        // println!("LEN:{}", self.elements.len());
+        let mut new: Vec<u8> = vec![0; self.elements.len() * 2];
+        // let mut new = vec![];
+        let mut j = 0;
         for i in 0..self.elements.len() - 1 {
-            new.push(self.elements[i]);
+            new[j] = self.elements[i];
+            j += 1;
+            // new.push(self.elements[i]);
             for p in &self.pairs {
                 if p.at[0] == self.elements[i] && p.at[1] == self.elements[i + 1] {
-                    new.push(p.insert);
+                    new[j] = p.insert;
+                    j += 1;
+                    // new.push(p.insert);
                 }
             }
         }
-        new.push(*self.elements.last().unwrap());
-
+        new[j] = self.elements[self.elements.len() - 1];
+        j += 1;
+        // new.push(*self.elements.last().unwrap());
+        new.truncate(j);
         self.elements = new;
     }
     fn value(&self) -> usize {
@@ -135,7 +180,22 @@ impl Day14 {
                 insert: pinsertv as u8,
             });
         });
-        Day14 { elements, pairs }
+        map.sort();
+        println!("elements:{}, pairs:{}\t{:?}", pairs.len(), map.len(), map);
+        for p in &pairs {
+            println!("{}", p.to_string());
+        }
+        let d = Day14 {
+            elements,
+            pairs,
+            n: map.len(),
+        };
+        let anal = d.freq_anal();
+        println!(
+            "====pair freq====\n{:?}\n====insert freq====\n{:?}",
+            anal.pairs, anal.inserts
+        );
+        d
     }
 }
 pub fn part1(lines: Vec<String>) -> String {
@@ -149,7 +209,7 @@ pub fn part1(lines: Vec<String>) -> String {
     // return ret;
 
     let mut ret = String::from("10 step:");
-    data.steps_new(16);
+    data.steps(10);
     //16:172844
     ret.push_str(&data.value().to_string());
     ret.push('\n');
