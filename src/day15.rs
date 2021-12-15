a55d60bb985e544730827cd9c5424ce58201ce05
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-15 19:59:18 +1000

    start day15

--- src/day15.rs
+++ src/day15.rs
@@ -1,59 +1,90 @@
-struct Day15 {}
+use std::{cmp::Ordering, collections::BinaryHeap, ops::RangeBounds};
+
+struct Day15 {
+    verticies: Vec<Vertex>,
+}
+
+#[derive(Clone, Eq, PartialEq)]
+struct Vertex {
+    weight: usize,
+    best: usize,
+    bestv: usize,
+    arcs: Vec<usize>,
+    i: usize,
+}
+impl Ord for Vertex {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that the we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other.best.cmp(&self.best)
+        // .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for Vertex {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 impl Day15 {
+    fn find_path(&mut self) -> usize {
+        let mut to_visit: BinaryHeap<&Vertex> = BinaryHeap::new();
+        let start: usize = 0;
+        let fin: usize = self.verticies.len();
+        to_visit.push(&self.verticies[start]);
+        while let Some(v) = to_visit.pop() {
+            for a in &v.arcs {
+                if self.verticies[*a].best > v.best + v.weight {
+                    self.verticies[*a].best = v.best + v.weight;
+                    self.verticies[*a].bestv = v.i;
+                }
+            }
+        }
+        0
+    }
+
     fn new(lines: Vec<String>) -> Self {
-        let mut map: Vec<char> = vec![];
-        let mut lines = lines.into_iter();
-        let mut raw_pairs: Vec<[usize; 3]> = vec![];
-        let mut elements: Vec<usize> = Vec::new();
-        lines.next().unwrap().trim().chars().for_each(|c| {
-            elements.push(match map.iter().position(|cm| cm == &c) {
-                Some(i) => i,
-                None => {
-                    map.push(c);
-                    map.len() - 1
-                }
-            })
-        });
-        //empty lines are ommited in data loader
-        lines.for_each(|line| {
-            let mut temp = line.split(" -> ");
-            let mut pairtext = temp.next().unwrap().chars();
-            let p1 = pairtext.next().unwrap();
-            let p2 = pairtext.next().unwrap();
-            let p1v = match map.iter().position(|c| c == &p1) {
-                Some(i) => i,
-                None => {
-                    map.push(p1);
-                    map.len() - 1
-                }
-            };
-            let p2v = match map.iter().position(|c| c == &p2) {
-                Some(i) => i,
-                None => {
-                    map.push(p2);
-                    map.len() - 1
-                }
-            };
-
-            let pinsert = temp.next().unwrap().chars().into_iter().next().unwrap();
-            let pinsertv = match map.iter().position(|c| c == &pinsert) {
-                Some(i) => i,
-                None => {
-                    map.push(pinsert);
-                    map.len() - 1
-                }
-            };
-            raw_pairs.push([p1v, p2v, pinsertv]);
-        });
-        let n = map.len();
-        let mut pairs: Vec<usize> = vec![0; n * n];
-        raw_pairs
+        let n = lines.len();
+        let twodboard: Vec<Vec<Vertex>> = lines
             .into_iter()
-            .for_each(|p| pairs[p[0] * n + p[1]] = p[2]);
+            .map(|s| {
+                s.trim()
+                    .split(" ")
+                    .map(|ss| Vertex {
+                        weight: ss.parse().unwrap(),
+                        best: 0,
+                        bestv: 0,
+                        arcs: vec![],
+                        i: 0,
+                    })
+                    .collect()
+            })
+            .collect();
+        let mut verticies: Vec<Vertex> = twodboard.into_iter().flatten().collect();
+        for x in 0..n {
+            for y in 0..n {
+                verticies[x * n + y].i = x * n + y;
+                if x > 0 {
+                    verticies[x * n + y].arcs.push((x - 1) * n + y)
+                }
+                if x < n - 1 {
+                    verticies[x * n + y].arcs.push((x + 1) * n + y)
+                }
+                if y > 0 {
+                    verticies[x * n + y].arcs.push(x * n + y - 1)
+                }
+                if y < n - 1 {
+                    verticies[x * n + y].arcs.push(x * n + y + 1)
+                }
+            }
+        }
 
-        let d = Day15 {};
-        d
+        Day15 {
+            verticies: twodboard.into_iter().flatten().collect(),
+        }
     }
 }
 pub fn part1(lines: Vec<String>) -> String {
