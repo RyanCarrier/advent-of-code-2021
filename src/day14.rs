54cd101e67fd2a4e65f1d93393dd334b530232f5
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-12-15 10:37:07 +1000

    clean up args and speed up day14

--- src/day14.rs
+++ src/day14.rs
@@ -1,31 +1,36 @@
-// #[derive(Clone, Copy)]
 struct Day14 {
     elements: Vec<usize>,
-    pairs: Vec<Vec<usize>>,
+    pairs: Vec<usize>,
     count: Vec<usize>,
+    n: usize,
 }
 
 impl Day14 {
     fn steps_new_new_new_new_new_new_new(&mut self, n: u8) {
         //You can predict what pairs will come next, so no need to track the entire string??
         self.elements.iter().for_each(|e| self.count[*e] += 1);
-        let mut pair_count = vec![vec![0 as usize; self.count.len()]; self.count.len()];
+        let cl = self.n;
+        let mut pair_count = vec![0 as usize; cl * cl];
         for i in 0..self.elements.len() - 1 {
-            pair_count[self.elements[i]][self.elements[i + 1]] += 1;
+            pair_count[self.elements[i] * cl + self.elements[i + 1]] += 1;
         }
+        let mut pair_count2: Vec<usize>;
         for _ in 0..n {
-            let mut pair_count2 = vec![vec![0; self.count.len()]; self.count.len()];
-            pair_count.iter().enumerate().for_each(|(pre, post_list)| {
-                post_list.iter().enumerate().for_each(|(post, pc)| {
-                    pair_count2[pre][self.pairs[pre][post]] += *pc;
-                    pair_count2[self.pairs[pre][post]][post] += *pc;
-                    self.count[self.pairs[pre][post]] += *pc;
-                });
-            });
+            pair_count2 = vec![0; cl * cl];
+            for pre in 0..cl {
+                for post in 0..cl {
+                    let pc = pair_count[pre * cl + post];
+                    let mid = self.pairs[pre * cl + post];
+                    pair_count2[pre * cl + mid] += pc;
+                    pair_count2[mid * cl + post] += pc;
+                    self.count[mid] += pc;
+                }
+            }
             pair_count = pair_count2;
         }
     }
 
+    #[allow(dead_code)]
     fn steps(&mut self, n: u8) {
         for i in 0..self.elements.len() - 1 {
             self.partial_step(n, self.elements[i], self.elements[i + 1])
@@ -36,7 +41,7 @@ impl Day14 {
         if n <= 0 {
             return;
         }
-        let mid = self.pairs[pre][post];
+        let mid = self.pairs[pre * self.n + post];
         self.count[mid] += 1;
         self.partial_step(n - 1, pre, mid);
         self.partial_step(n - 1, mid, post);
@@ -93,27 +98,26 @@ impl Day14 {
             };
             raw_pairs.push([p1v, p2v, pinsertv]);
         });
-        let mut pairs: Vec<Vec<usize>> = vec![vec![0; map.len()]; map.len()];
-        raw_pairs.into_iter().for_each(|p| pairs[p[0]][p[1]] = p[2]);
+        let n = map.len();
+        let mut pairs: Vec<usize> = vec![0; n * n];
+        raw_pairs
+            .into_iter()
+            .for_each(|p| pairs[p[0] * n + p[1]] = p[2]);
 
         let d = Day14 {
             elements,
-            count: vec![0; map.len()],
+            count: vec![0; n],
             pairs,
+            n,
         };
         d
     }
 }
 pub fn part1(lines: Vec<String>) -> String {
     let mut data = Day14::new(lines);
-    let steps = 40;
-    let mut ret = format!("step {}:", steps);
-    // data.steps(10);
-    // 16:172844
-    // data.steps(steps);
-    data.steps_new_new_new_new_new_new_new(steps);
-    //40:2857918149 TOO LOW
-    //40:2857918149 (I think it was actually a 30 iter tho (confirmed lol))
+    data.steps_new_new_new_new_new_new_new(10);
+    let mut ret = format!("p1: {}, p2: ", &data.value().to_string());
+    data.steps_new_new_new_new_new_new_new(30);
     ret.push_str(&data.value().to_string());
     ret
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use ansi_term::Style;
-use std::time::{Duration, Instant};
+use std::{
+    env,
+    time::{Duration, Instant},
+};
 mod day1;
 mod day10;
 mod day11;
@@ -17,7 +20,6 @@ mod day9;
 mod util;
 
 fn main() {
-    let run_benchmark = !true;
     let fns = vec![
         [day1::part1, day1::part2],
         [day2::part1, day2::part2],
@@ -34,74 +36,125 @@ fn main() {
         [day13::part1, day13::part2],
         [day14::part1, day14::part2],
     ];
-    if run_benchmark {
+    process_args(fns);
+}
+
+fn process_args(fns: Vec<[fn(Vec<String>) -> String; 2]>) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
         benchmark(fns);
         return;
     }
-    let day = fns.len();
-    run_specific(fns, day);
-    // run_bench(fns, 13, 2000);
+    let day: usize;
+    match args[1].as_str() {
+        "bench" => return benchmark(fns),
+        _ => {
+            day = match args[1].parse::<usize>() {
+                Ok(day) => day,
+                Err(_) => panic!("incorrect syntax"),
+            };
+        }
+    }
+    if args.len() < 3 {
+        return run_specific(fns, day);
+    }
+    match args[2].as_str() {
+        "bench" => {
+            if args.len() < 4 {
+                panic!("need repeats")
+            }
+            let repeats = match args[3].parse::<usize>() {
+                Ok(repeats) => repeats,
+                Err(_) => panic!("incorrect syntax"),
+            };
+            return bench_specific(fns, day, repeats);
+        }
+        _ => {
+            match &args[2].parse::<usize>() {
+                Ok(part) => {
+                    if [1, 2].contains(part) {
+                        return run_single(fns, day, *part - 1);
+                    }
+                    panic!("incorrect syntax");
+                }
+                Err(_) => panic!("incorrect syntax"),
+            };
+        }
+    }
 }
 
-fn run_specific(fns: Vec<[fn(Vec<String>) -> String; 2]>, n: usize) {
+fn run_specific(fns: Vec<[fn(Vec<String>) -> String; 2]>, day: usize) {
+    let input_test = util::get_test_from_file(day);
     let start = Instant::now();
-    println!(
-        "day{}part{}test:\t{}",
-        n,
-        1,
-        fns[n - 1][0](util::get_test_from_file(n))
-    );
+    println!("day{}part{}test:\t{}", day, 1, fns[day - 1][0](input_test));
     let test_duration = start.elapsed();
-    println!(
-        "day{}part{}:\t{}",
-        n,
-        1,
-        fns[n - 1][0](util::get_from_file(n))
-    );
-    let part1_duration = start.elapsed() - test_duration;
-    println!(
-        "day{}part{}:\t{}",
-        n,
-        2,
-        fns[n - 1][1](util::get_from_file(n))
-    );
-    let total_duration = start.elapsed();
+    let input = util::get_from_file(day);
+    let inputp2 = input.clone();
+    let part1_start = Instant::now();
+    println!("day{}part{}:\t{}", day, 1, fns[day - 1][0](input));
+    let part1_duration = part1_start.elapsed();
+    let part2_start = Instant::now();
+    println!("day{}part{}:\t{}", day, 2, fns[day - 1][1](inputp2));
+    let part2_duration = part2_start.elapsed();
     println!(
         "Completed in {}\t(p1t:{}, p1:{}, p2:{})",
-        util::format_duration(total_duration),
+        util::format_duration(test_duration + part1_duration + part2_duration),
         util::format_duration(test_duration),
         util::format_duration(part1_duration),
-        util::format_duration(total_duration - test_duration - part1_duration)
+        util::format_duration(part2_duration)
     );
 }
 
-#[allow(dead_code)]
-fn run_bench(fns: Vec<[fn(Vec<String>) -> String; 2]>, n: usize, repeats: usize) {
-    let input = util::get_from_file(n);
-    let inputs: Vec<Vec<String>> = (0..repeats).map(|_| input.clone()).collect();
+fn run_single(fns: Vec<[fn(Vec<String>) -> String; 2]>, day: usize, part: usize) {
+    let input = util::get_from_file(day);
+    let start = Instant::now();
+    let result = fns[day - 1][part](input);
+    let duration = start.elapsed();
+    println!(
+        "day{}part{}:\t{}\nCompleted in {}",
+        day,
+        part + 1,
+        result,
+        util::format_duration(duration),
+    );
+}
+
+fn bench_specific(fns: Vec<[fn(Vec<String>) -> String; 2]>, day: usize, repeats: usize) {
+    let input = util::get_from_file(day);
+    let mut inputs: Vec<Vec<String>> = (0..repeats).map(|_| input.clone()).collect();
     let total_start = Instant::now();
     for input in inputs {
-        let _ = fns[n - 1][0](input);
+        fns[day - 1][0](input);
     }
     let duration1 = total_start.elapsed();
 
-    let inputs: Vec<Vec<String>> = (0..repeats).map(|_| input.clone()).collect();
+    inputs = (0..repeats).map(|_| input.clone()).collect();
     let start2 = Instant::now();
     for input in inputs {
-        let _ = fns[n - 1][1](input);
+        fns[day - 1][1](input);
     }
     let duration2 = start2.elapsed();
     let total_duration = duration1 + duration2;
-    println!("Part1 in {}μs", duration1.as_micros() / repeats as u128);
-    println!("Part2 in {}μs", duration2.as_micros() / repeats as u128);
     println!(
-        "Completed in {}μs",
-        total_duration.as_micros() / repeats as u128
+        "Day {} Part1 in {}",
+        day,
+        util::format_duration(duration1.div_f64(repeats as f64))
     );
     println!(
-        "p1 result:{}, p2 result:{}",
-        fns[n - 1][0](input.clone()),
-        fns[n - 1][1](input.clone())
+        "Day {} Part2 in {}",
+        day,
+        util::format_duration(duration2.div_f64(repeats as f64))
+    );
+    println!(
+        "Completed day {} in {}",
+        day,
+        util::format_duration(total_duration.div_f64(repeats as f64))
+    );
+    println!(
+        "Day {} p1 result:{}, p2 result:{}",
+        day,
+        fns[day - 1][0](input.clone()),
+        fns[day - 1][1](input.clone())
     );
 }
 
